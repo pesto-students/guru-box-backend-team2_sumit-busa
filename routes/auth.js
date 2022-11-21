ce41097b49965899e27789179a5cc7bf09f1c830
chacha73 <82140773+krishnkant-pyt@users.noreply.github.com>
2022-11-21 23:03:38 +0530

    fixs auth APIs
    fixs auth APIs

--- routes/auth.js
+++ routes/auth.js
@@ -4,5 +4,7 @@ const authController = require('../controllers/auth.js');
 
 router.post('/login', authController.login);
 router.post('/signup', authController.signup);
+router.post('/refresh', authController.refresh);
+router.post('/logout', authController.logout);
 
 module.exports = router;
